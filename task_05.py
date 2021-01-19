33393306657a45262e8a3966f9bbb42cfdcd0585
evogul <evogul@mail.ru>
2021-01-19 08:37:10 +0300

    слегка оптимизировал задачу 5

--- task_05.py
+++ task_05.py
@@ -16,19 +16,32 @@
 
 my_list = [7, 5, 3, 3, 2]
 num = int(input('input num: '))
-while num>0:
-     if my_list.count(num)>0:                                       #если такой балл найден в списке,
-         my_list.insert(my_list.index(num)+my_list.count(num),num)  #вставляем его последним из таких же
-     elif num>my_list[0]:                                           #если балл больше, чем первый в списке
-        my_list.insert(0,num)                                       #вставляем его в начало списка
-     else:
-        f_found = False
-        for i in range(num,0,-1):                                   #перебор баллов в меньшую сторону от введенного
-            if my_list.count(i) > 0:                                #если найден в списке
-                my_list.insert(my_list.index(i), num)               #добавляем введенный перед ним
-                f_found = True
+# while num>0:
+#      if my_list.count(num)>0:                                       #если такой балл найден в списке,
+#          my_list.insert(my_list.index(num)+my_list.count(num),num)  #вставляем его последним из таких же
+#      elif num>my_list[0]:                                           #если балл больше, чем первый в списке
+#         my_list.insert(0,num)                                       #вставляем его в начало списка
+#      else:
+#         f_found = False
+#         for i in range(num,0,-1):                                   #перебор баллов в меньшую сторону от введенного
+#             if my_list.count(i) > 0:                                #если найден в списке
+#                 my_list.insert(my_list.index(i), num)               #добавляем введенный перед ним
+#                 f_found = True
+#                 break
+#         if f_found == False:                                        #если ничего не найдено
+#             my_list.append(num)                                     #добавляем в конец списка
+#      print(my_list)
+#      num = int(input('input num: '))
+while num!=0:
+    if num<my_list[len(my_list)-1]:
+        my_list.append(num)
+    else:
+        for el in my_list:
+            if num>el:
+                my_list.insert(my_list.index(el),num)
                 break
-        if f_found == False:                                        #если ничего не найдено
-            my_list.append(num)                                     #добавляем в конец списка
-     print(my_list)
-     num = int(input('input num: '))
+            elif num==el:
+                my_list.insert(my_list.index(el)+my_list.count(el), num)
+                break
+    print(my_list)
+    num = int(input('input num: '))
